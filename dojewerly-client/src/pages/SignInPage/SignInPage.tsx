711a2f757730a4ac8733146e89c97914a3b451f2
Viachaslau Kulbitski <fixrapdok@gmail.com>
2023-07-31 01:55:44 +0300

    add: image

--- dojewerly-client/src/pages/SignInPage/SignInPage.tsx
+++ dojewerly-client/src/pages/SignInPage/SignInPage.tsx
@@ -3,28 +3,34 @@ import styles from './SignInPage.module.css';
 import variables from '../../variables.module.css'
 import SignInForm from '../../components/Auntefication/SignInForm';
 import Button from '../../components/Button/Button';
+import banner from '../../assets/images/banner-1.jpg';
 
 const SignInPage = memo(() => {
 
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.heading}>
-          <h1>Hello, Let's Sign In</h1>
-          <div
-            className={variables.description}
-            style={{color: 'var(--grey-1)'}}>
-              Please sign in to your DoJewerly Account.
-            </div>
+        <div className={styles.imageContainer}>
+            <img src={banner} alt="Banner" /> {/* Добавьте элемент img */}
         </div>
-        <SignInForm />
-        <Button
-          type="button"
-          variant="text"
-          size="small"
-          fullWidth={true} 
-          text="CREATE AN ACCOUNT"
-          iconRight="arrowRight"/>
+        <div className={styles.form}>
+          <div className={styles.heading}>
+            <h1>Hello, Let's Sign In</h1>
+            <div
+              className={variables.description}
+              style={{color: 'var(--grey-1)'}}>
+                Please sign in to your DoJewerly Account.
+              </div>
+          </div>
+          <SignInForm />
+          <Button
+            type="button"
+            variant="text"
+            size="small"
+            fullWidth={true} 
+            text="CREATE AN ACCOUNT"
+            iconRight="arrowRight"/>
+        </div>
       </div>
     </>
   );
